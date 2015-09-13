5f6d3c9c4da5500a4d584c61a8a9774b80ab7ab7
Brian O'Connor <boconnor@trunkclub.com>
2015-09-13 17:41:38 -0500

    Code Complete

---
+++ plot2.R
@@ -0,0 +1,14 @@
+#setwd("/Users/boc/Dropbox/Coursera/ExploratoryDataAnalysis/Assignment1")
+
+#read data by date range
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+#str(subSetData)
+
+#plot data
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
